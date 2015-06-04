6c61dcf8c945686e9da2cba822bd98b24cffc975
Vishwas Kharate <vishwas.kharate@pragtech.co.in>
2015-06-04 16:59:48 +0530

    Removed some images from flash

--- js/vegas.js
+++ js/vegas.js
@@ -1,1 +1,1 @@
-$(function(){$.vegas("slideshow",{backgrounds:[{src:"img/bg/bg1.jpg",fade:1e3},{src:"img/bg/bg2.jpg",fade:1e3},{src:"img/bg/bg4.jpg",fade:1e3},{src:"img/bg/bg11.jpg",fade:1e3},{src:"img/bg/bg10.jpg",fade:1e3}]})});
+$(function(){$.vegas("slideshow",{backgrounds:[{src:"img/bg/bg1.jpg",fade:1e3},{src:"img/bg/bg2.jpg",fade:1e3},{src:"img/bg/bg10.jpg",fade:1e3}]})});
